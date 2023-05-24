e683e286c2d1256361ae2a60051f4958ead2836c
Deflandrenassim <nassim.def@gmail.com>
2023-05-24 09:58:26 +0200

    composant Title

--- project/src/app/page.js
+++ project/src/app/page.js
@@ -1,13 +1,17 @@
 'use client';
 import { TextField } from "../component/Textfield"
+import { Title } from "@/component/Title";
 
 export default function Home() {
 
   const handleEvent = (e) => {
+    e.preventDefault();
     console.log(e.target.value)
   }
+
   return (
     <main>
+      <Title> Find To Profil Github</Title>
       <TextField placeholder="Search Profil" onChange={handleEvent} />
     </main>
   )
